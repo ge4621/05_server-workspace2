87837d3215256128ac4ab0fff4252733550b488d
ge4621 <ge4621@naver.com>
2023-08-07 21:48:47 +0900

    ..
    ..

---
+++ jspProject/SEVER수업용계정_SERVER.sql
@@ -0,0 +1,17 @@
+--로그인 요청 시 실행 될 sql문
+SELECT
+      USER_NO
+    , USER_ID
+    , USER_PWD
+    , USER_NAME
+    , PHONE
+    , EMAIL
+    , ADDRESS
+    , interest
+    , ENROLL_DATE
+    , MODIFY_DATE
+    , STATUS
+FROM MEMBER
+WHERE USER_ID =?
+AND USER_PWD =?
+AND STATUS = 'Y';
